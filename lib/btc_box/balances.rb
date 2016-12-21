0f78993107c283ca7ce22ed256655ad2430b886c
Cuong Vu <cuongvu0103@gmail.com>
2016-12-21 14:40:52 +0700

    Update Balances.all

--- lib/btc_box/balances.rb
+++ lib/btc_box/balances.rb
@@ -2,11 +2,8 @@ module BtcBox
   module Balances
     def self.all
       BtcBox.sanity_check!
-      balances = BtcBox::Net.post("/v1/balance", {}, true)
-
-      JSON.parse(balances).map do |balance|
-        BtcBox::Balance.new(balance)
-      end
+      balance = BtcBox::Net.post("/v1/balance", {}, true)
+      BtcBox::Balance.new(balance)
     end
   end
 end
